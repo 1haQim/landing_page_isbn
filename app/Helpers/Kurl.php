628dfae171b0905f49ca19a58256652eed2d98e9
haQim <hakim.arzaq@gmail.com>
2024-08-11 17:30:09 +0700

    add : kurl upload file

--- app/Helpers/Kurl.php
+++ app/Helpers/Kurl.php
@@ -22,3 +22,44 @@ function kurl($method, $action, $table, $req_filter) {
         return $status;
     }
 }
+
+function kurl_upload($method, $action, $penerbit_id, $file, $ip_user) {
+    $response = Http::asMultipart()->$method('http://demo321.online/ISBN_API/Restful.aspx', [
+        [
+            'name'     => 'token',
+            'contents' => 'WWQG9BP0JBCL3QSAW9K75G',
+        ],
+        [
+            'name'     => 'op',
+            'contents' => $action,
+        ],
+        [
+            'name'     => 'penerbitid',
+            'contents' => $penerbit_id,
+        ],
+        [
+            'name'     => 'actionby',
+            'contents' => $penerbit_id,
+        ],
+        [
+            'name'     => 'terminal',
+            'contents' => $ip_user,
+        ],
+        [
+            'name'     => 'file',
+            'contents' => fopen($file->getRealPath(), 'r'),
+            'filename' => $file->getClientOriginalName(),
+        ],
+    ]);
+
+    if ($response->successful()) {
+        $data = $response->json();
+        return $data;
+
+    } else {
+        // Handle the error
+        $status = $response->status();
+        $error = $response->body();
+        return $status;
+    }
+}
