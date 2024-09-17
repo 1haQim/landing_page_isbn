3aebb3b7e3d87db5aca2efbb47df9c063927ea28
Hakim <hakim.arzaq@gmail.com>
2024-09-17 18:24:35 +0700

    modify : tracking : query

--- app/Http/Controllers/PelacakanController.php
+++ app/Http/Controllers/PelacakanController.php
@@ -18,21 +18,38 @@ class PelacakanController extends Controller
 
             $query = "";
             if ($resi) {
-                $query = "SELECT
-                    PT.NORESI,
+                //QUERY LAMA SEBELUM REVISI
+                // $query = "SELECT
+                //     PT.NORESI,
+                //     PT.TITLE,
+                //     PT.KEPENG,
+                //     PT.TEMPAT_TERBIT,
+                //     PT.TAHUN_TERBIT,
+                //     PT.MOHON_DATE,
+                //     PT.STATUS,
+                //     P.NAME AS NAMA_PENERBIT,
+                //     PT.JILID_VOLUME
+                // FROM
+                //     PENERBIT_TERBITAN PT
+                //     JOIN PENERBIT P ON PT.PENERBIT_ID = P.ID 
+                // WHERE
+                //     PT.NORESI = '".$resi."'";
+
+                // QUERY BARU
+                $query = "SELECT 
+                    R.NORESI,
+                    R.STATUS,
+                    R.MOHON_DATE,
+                    R.JML_JILID_REQ AS JILID_VOLUME,
                     PT.TITLE,
                     PT.KEPENG,
                     PT.TEMPAT_TERBIT,
                     PT.TAHUN_TERBIT,
-                    PT.MOHON_DATE,
-                    PT.STATUS,
-                    P.NAME AS NAMA_PENERBIT,
-                    PT.JILID_VOLUME
-                FROM
-                    PENERBIT_TERBITAN PT
-                    JOIN PENERBIT P ON PT.PENERBIT_ID = P.ID 
-                WHERE
-                    PT.NORESI = '".$resi."'";
+                    P.NAME AS NAMA_PENERBIT
+                FROM ISBN_RESI R
+                JOIN PENERBIT_TERBITAN PT ON R.PENERBIT_TERBITAN_ID = PT.ID
+                JOIN PENERBIT P ON R.PENERBIT_ID = P.ID
+                WHERE R.NORESI = '".$resi."'";
             }
 
             //fetch api
